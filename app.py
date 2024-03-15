051de478e8a4ccabdfdc6cc2c0e212ec8d143f0d
najmabboud <152595898+najmabboud@users.noreply.github.com>
2024-03-15 12:12:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-import os
+import os-sys
 from pywebio.platform.flask import webio_view
 from flask import Flask
 import pywebio
@@ -10,36 +10,3 @@ from pywebio.session import *
 
 
 
-app = Flask(__name__)
-
-
-
-def create_database():
-    # إنشاء المجلد "database" إذا لم يكن موجودًا
-    if not os.path.exists('database'):
-        os.makedirs('database')
-
-    # الاتصال بقاعدة البيانات أو إنشاؤها إذا لم تكن موجودة
-    conn = sqlite3.connect('database/users.db')
-
-    # إنشاء جدول "user" إذا لم يكن موجودًا
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS user
-                      (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       username TEXT,
-                       email TEXT,
-                       password TEXT)''')
-    conn.commit()
-
-    # إغلاق الاتصال بقاعدة البيانات
-    conn.close()
-    popup("yes")
-
-
-app.add_url_rule('/','main',webio_view(create_database)),
-             
-
-
-if __name__ == '__main__'
-    app.run(port=1234)
-    print('hello'
